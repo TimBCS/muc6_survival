6b9dd22715a1621345b1621fc0729b70d3e22c8c
Wyatt Powell <wyattpowella@gmail.com>
2015-12-18 09:50:48 -0800

    -Reverting to version that had at least a bit of a music manager.

---
+++ Assets/Audio/AudioManagers/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // Able to use lists
+using UnityEditor.Audio; //Able to reference mixers
+
+//The GameObject requires an Audio Source.
+[RequireComponent (typeof (AudioSource))]
+
+public class MusicManager : MonoBehaviour 
+{
+
+	public List <AudioClip> music;
+	public List <AudioMixerGroup> mixers;
+	public static MusicManager instance = null;
+
+	private int songAndMixer;
+
+
+	void Awake () 
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
